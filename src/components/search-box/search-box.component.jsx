f4a4f47ac07982e9ff45b9054522ea0685d5be04
Chen An(CHTTL) <openan7@gmail.com>
2022-04-27 17:18:51 +0800

    Change all component to hook

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,13 +1,12 @@
-import { Component } from "react";
 import './search-box.styles.css';
 
 
-class SearchBox extends Component {
-    render(){
-        return(
-            <input className={this.props.className} type='search' placeholder={this.props.placeholder} onChange={this.props.onChange} />
-        )
-    }
+const SearchBox = ( {className , placeholder , onChange} ) => {
+    return(
+        <input className={className} type='search' placeholder={placeholder} onChange={onChange} />
+    )
 }
+
+
 
 export default SearchBox;
